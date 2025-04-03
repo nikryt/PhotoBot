23618d151a0f0d82bf845baa85d35bca26f26acb
nikryt <nikryt@nikryt.ru>
2025-04-03 16:54:02 +0500

    ОТправка фотографии в инструкции к Nikon

--- app/handlers_help.py
+++ app/handlers_help.py
@@ -5,7 +5,7 @@ from aiogram.filters import Command
 import Texts
 from aiogram import Router, F, Bot
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InputFile
 
 from app.handlers import send_typing_and_message, mes_user_history
 from app.keyboards import create_inline_keyboard
@@ -16,8 +16,8 @@ help_router = Router()
 async def  cmd_help(message: Message, state: FSMContext, bot: Bot):
     await mes_user_history(message, state)
     keyboard = await create_inline_keyboard(
-        Texts.Help.PHOTO_HELP,
-        Texts.Help.BILD_HELP,
+        Texts.Buttons.PHOTO_HELP,
+        Texts.Buttons.BILD_HELP,
         callback_data={
             0: "Photo",
             1: "Bild"
@@ -35,12 +35,13 @@ async def  cmd_help(message: Message, state: FSMContext, bot: Bot):
 async def back_to_main_help(callback: CallbackQuery, bot: Bot, state: FSMContext):
     # Создаем оригинальную клавиатуру из команды /help
     keyboard = await create_inline_keyboard(
-        Texts.Help.PHOTO_HELP,
-        Texts.Help.BILD_HELP,
+        Texts.Buttons.PHOTO_HELP,
+        Texts.Buttons.BILD_HELP,
         callback_data={0: "Photo", 1: "Bild"}
     )
-
-    await callback.message.edit_text(text=Texts.Help.MAIN, reply_markup=keyboard)
+    # Удаляем исходное сообщение с кнопкой
+    await callback.message.delete()
+    await callback.message.answer(text=Texts.Help.MAIN, reply_markup=keyboard)
     await callback.answer()
 
 # Обработчик нажатия на кнопку "Photo"
@@ -49,8 +50,8 @@ async def photo_help(callback: CallbackQuery, bot: Bot, state: FSMContext):
     # Создаем новую клавиатуру
     new_keyboard = await create_inline_keyboard(
         Texts.Help.CAMERA_SETUP,
-        Texts.Help.ADD_MARKS,
-        Texts.Help.BACK,
+        Texts.Buttons.ADD_MARKS,
+        Texts.Buttons.BACK,
         callback_data={
             0: "camera_select",
             1: "add_marks",
@@ -70,11 +71,11 @@ async def photo_help(callback: CallbackQuery, bot: Bot, state: FSMContext):
 @help_router.callback_query(F.data == "camera_select")
 async def camera_select_vendor(callback: CallbackQuery):
     new_keyboard = await create_inline_keyboard(
-        Texts.Help.CAMERA_NIKON,
-        Texts.Help.CAMERA_CANON,
-        Texts.Help.CAMERA_SONY,
-        Texts.Help.CAMERA_FUJIFILM,
-        Texts.Help.BACK,
+        Texts.Buttons.CAMERA_NIKON,
+        Texts.Buttons.CAMERA_CANON,
+        Texts.Buttons.CAMERA_SONY,
+        Texts.Buttons.CAMERA_FUJIFILM,
+        Texts.Buttons.BACK,
         callback_data={
             0: "nikon",
             1: "canon",
@@ -88,11 +89,12 @@ async def camera_select_vendor(callback: CallbackQuery):
     await callback.message.edit_text(text=Texts.Help.CAMERA_SELECT, parse_mode=ParseMode.HTML, reply_markup=new_keyboard)
     await callback.answer()
 
+
 @help_router.callback_query(F.data == "nikon")
 async def camera_setup_nikon(callback: CallbackQuery):
     new_keyboard = await create_inline_keyboard(
-        Texts.Help.ADD_MARKS,
-        Texts.Help.BACK,
+        Texts.Buttons.ADD_MARKS,
+        Texts.Buttons.BACK,
         callback_data={
             0: "add_marks",
             1: "help_back"
@@ -100,14 +102,26 @@ async def camera_setup_nikon(callback: CallbackQuery):
         sizes=(2,)
     )
 
-    await callback.message.edit_text(text=Texts.Help.CAMERA_SETUP_NIKON, parse_mode=ParseMode.HTML, reply_markup=new_keyboard)
+    # Удаляем исходное сообщение с кнопкой
+    await callback.message.delete()
+
+    # Загружаем фото (используйте правильный путь/URL/file_id)
+    photo = "AgACAgIAAxkBAAI3u2fudWaLos4eWNunSNGXEA1-NcENAALd8DEbzrpwS5f9BFc3MscyAQADAgADbQADNgQ"  # Или URL, file_id
+
+    # Отправляем новое сообщение с фото
+    await callback.message.answer_photo(
+        photo=photo,
+        caption=Texts.Help.CAMERA_SETUP_NIKON,  # Текст под фото
+        parse_mode=ParseMode.HTML,
+        reply_markup=new_keyboard  # Кнопки под фото
+    )
     await callback.answer()
 
 @help_router.callback_query(F.data == "canon")
 async def camera_setup_nikon(callback: CallbackQuery):
     new_keyboard = await create_inline_keyboard(
-        Texts.Help.ADD_MARKS,
-        Texts.Help.BACK,
+        Texts.Buttons.ADD_MARKS,
+        Texts.Buttons.BACK,
         callback_data={
             0: "add_marks",
             1: "help_back"
